c6945a1784e46da974426165a29f1756f151201e
Alejandro <agee.baltazar@gmail.com>
2026-06-12 16:01:40 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Introduction
+This is a basic template for ESP32 base devices which configured two GPIOS, the push button and LED.
+Created for use with VSCode on the PlatformIO extension with espidf framework.
+
+Looking for a simple breakdown of the code? [Check Here](https://medium.com/@agee.baltazar/a-beginners-guide-to-esp32-ide-setup-with-vscode-and-platformio-from-a-beginner-4e3e3a4a9855)
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_PIN    GPIO_NUM_1
+#define BUTTON_PIN GPIO_NUM_0 
+#define PUMP_OUTPUT GPIO_NUM_4
+
+void app_main(void) {
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    // Button input with pull-up
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLUP_ONLY);
+
+    while (1) {
+        if (gpio_get_level(BUTTON_PIN) == 0) {  
+            gpio_set_level(LED_PIN, 1);
+            gpio_set_level(BUTTON_PIN, 1);
+        } else {
+            gpio_set_level(LED_PIN, 0);
+            gpio_set_level(BUTTON_PIN, 0);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));  
+    }
+}
